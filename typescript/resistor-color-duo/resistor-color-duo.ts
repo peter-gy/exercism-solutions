93abc85c17c8908c585ee7c35589841a4d516872
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2022-04-16 09:50:31 +0200

    solution(typescript): resistor-color-duo

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,27 @@
+type Color =
+  | "black"
+  | "brown"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "violet"
+  | "grey"
+  | "white";
+const colorCodes: Record<Color, number> = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export function decodedValue(colors: Color[]): number {
+  return 10 * colorCodes[colors[0]] + colorCodes[colors[1]];
+}
